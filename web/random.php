2c88a4047e5239f434a78235aac6a08f4d16aa8d
ktrips <kenichi_yoshida@hotmail.com>
2016-12-08 18:16:33 +0900

    Update random.php

--- web/random.php
+++ web/random.php
@@ -1,4 +1,5 @@
 <?php
 
-img = "https://" . $_SERVER['SERVER_NAME'] . "/IMG_" . rand(1426,1568) . ".PNG"
-print(img)
+img = "https://" . $_SERVER['SERVER_NAME'] . "/IMG_" . rand(1426,1568) . ".PNG";
+img_url = "<img src=" . img . ">";
+print(img_url);
