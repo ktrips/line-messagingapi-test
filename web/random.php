4520631ea5591f69e85d1f22641909d474bb6339
ktrips <kenichi_yoshida@hotmail.com>
2016-12-08 18:23:15 +0900

    Update random.php

--- web/random.php
+++ web/random.php
@@ -1,6 +1,6 @@
 <?php
 
-$img = "https://ktribot.herokuapp.com/IMG_" . rand(1426,1568) . ".PNG";
+$img = "https://ktribot.herokuapp.com/IMG_" . rand(1426,1468) . ".PNG";
   //. $_SERVER['SERVER_NAME'] 
 $img_url = "<img src=" . $img . ">";
 print $img_url;
